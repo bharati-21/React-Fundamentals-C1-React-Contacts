c2c7777b611f0f4f33a17bf6bdd3862e7b0b1485
Bharati Subramanian <bharatisharada@gmail.com>
2022-11-23 11:46:36 +0530

    feat: search by contact name

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,33 +1,75 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const ListContacts = ({ contacts, removeContact }) => {
+	const [searchText, setSearchText] = useState("");
+
 	const handleRemoveContact = (contact) => {
 		removeContact(contact);
 	};
 
+	const handleSearchTextChange = (e) => {
+        const text = e.target.value;
+		setSearchText(text.trim());
+	};
+
+	const getFilteredContacts = () => {
+		if (!searchText) {
+			return contacts;
+		}
+
+		return contacts.filter((contact) =>
+			contact.name.toLowerCase().includes(searchText.toLowerCase())
+		);
+	};
+
+	const resetSearchText = () => setSearchText("");
+
+	const filteredContacts = getFilteredContacts();
+
 	return (
-		<ol className="contact-list">
-			{contacts.map((contact) => (
-				<li key={contact.id} className="contact-list-item">
-					<div
-						className="contact-avatar"
-						style={{
-							backgroundImage: `url(${contact.avatarURL})`,
-						}}
-					></div>
-					<div className="contact-details">
-						<p className="contact-name">{contact.name}</p>
-						<p className="contact-handle">{contact.handle}</p>
-					</div>
-					<button
-						className="contact-remove"
-						onClick={() => handleRemoveContact(contact)}
-					>
-						Remove
-					</button>
-				</li>
-			))}
-		</ol>
+		<div className="list-contacts">
+			<div className="list-contacts-top">
+				<input
+					type="text"
+					className="search-contacts"
+					placeholder="Search Contacts"
+					value={searchText}
+					onChange={handleSearchTextChange}
+				/>
+			</div>
+			{filteredContacts.length !== contacts.length && (
+				<div className="showing-contacts">
+					<span>
+						Now showing {filteredContacts.length} of{" "}
+						{contacts.length}
+					</span>
+					<button onClick={resetSearchText}>Show all</button>
+				</div>
+			)}
+			<ol className="contact-list">
+				{filteredContacts.map((contact) => (
+					<li key={contact.id} className="contact-list-item">
+						<div
+							className="contact-avatar"
+							style={{
+								backgroundImage: `url(${contact.avatarURL})`,
+							}}
+						></div>
+						<div className="contact-details">
+							<p className="contact-name">{contact.name}</p>
+							<p className="contact-handle">{contact.handle}</p>
+						</div>
+						<button
+							className="contact-remove"
+							onClick={() => handleRemoveContact(contact)}
+						>
+							Remove
+						</button>
+					</li>
+				))}
+			</ol>
+		</div>
 	);
 };
 
